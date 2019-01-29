15018d81cc937c260a9016e5ceac15cf7fdcc56f
Daan <bluepixel2017@gmail.com>
2019-01-29 10:21:15 +0100

    Second upload of complete project

--- src/Opdracht2_3.java
+++ src/Opdracht2_3.java
@@ -9,7 +9,7 @@ public class Opdracht2_3 extends Applet {
         g.setColor(Color.yellow);
         g.drawRect(20, 20, 100, 50);
         g.fillRect(20, 80, 100, 50);
-        //test
+       
 
 
 
