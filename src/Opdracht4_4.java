3959175056cdfc328da91106d48d1c133a4cbeac
Daan <bluepixel2017@gmail.com>
2019-01-29 11:01:29 +0100

    Second upload of complete project

---
+++ src/Opdracht4_4.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.applet.*;
+
+
+public class Opdracht4_4 extends Applet {
+
+    public void init(){
+
+
+
+    }
+
+
+    public void paint(Graphics g) {
+
+    }
+}
