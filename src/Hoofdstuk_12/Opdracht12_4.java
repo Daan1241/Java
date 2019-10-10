405b1e6c5b0e0e830468223c83ac77f22fd6fb72
daan1241 <daanklein75@gmail.com>
2019-10-10 11:01:51 +0100

    -

---
+++ src/Hoofdstuk_12/Opdracht12_4.java
@@ -0,0 +1,18 @@
+package Hoofdstuk_12;
+
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+public class Opdracht12_4 extends Applet{
+
+
+    TextField tekstveld = new TextField();
+    public void init(){
+        add(tekstveld);
+
+
+    }
+
+
+}
